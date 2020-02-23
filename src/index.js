cb7661e85436db8030082f70afd96b6a8eb9614e
Alyssa Clarke <alyssa.clarke00@gmail.com>
2020-02-23 10:50:51 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const list = document.querySelector('#tasks')
+  let listItem = document.createElement('ul')
+
+  const task = document.getElementById('create-task-form')
+
+  task.addEventListener('submit', event => {
+    event.preventDefault() // stops the submit from calling an HTML request
+
+    let taskInfo = document.querySelector('#new-task-description').value
+
+    const newLi = document.createElement('li')
+    newLi.innerHTML = taskInfo + "<button class='delete'>delete me</button>"
+    list.appendChild(newLi)
+
+    const deleteButton = newLi.querySelector('.delete')
+
+    deleteButton.addEventListener('click', event => {
+      newLi.remove()
+    })
+
+
+  })
+
+
+
+
 });
+ 
